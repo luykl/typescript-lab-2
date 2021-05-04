9adbd4c740cca6a8841c0fc366d517ac071ca84c
luykl <leah.logsin@gmail.com>
2021-05-04 15:48:27 -0400

    updated index

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 import { Greeter } from "./greeter";
+import { JavaScriptGreeter } from "./javascript-greeter";
+import { LoudGreeter } from "./loud-greeter";
+import { HTMLGreeter } from "./html-greeter";
 
-console.log((new Greeter("Hi")).greeting);
+console.log( (new Greeter("Hi")).greet("Leah") );
+console.log( (new JavaScriptGreeter("Hi")).greet("Leah") );
+console.log( (new LoudGreeter("Hi")).greet("Leah") );
+console.log( (new HTMLGreeter("Hi", "strong")).greet("Leah") );
